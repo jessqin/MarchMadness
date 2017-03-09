a8ffad1bd68bb36b993ba2afc00af449d2a8afde
jessqin <jessicaq319@gmail.com>
2017-03-09 14:29:30 -0500

    fifth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,13 +9,12 @@ public class Main {
     public static void main(String[] args) throws IOException
     {
         String csvFile = "TourneyCompactResults.csv";
-        //BufferedReader br = new BufferedReader(new FileReader(new File(csvFile)));
         Scanner keyIn = new Scanner(new File(csvFile));
 
 
         int maxIndx = -1;
         ArrayList<Team> stats = new ArrayList();
-        String [] sStats = new String[3000];
+        String [] sStats = new String[10000];
         while(keyIn.hasNextLine())
         {
             maxIndx++;
@@ -38,16 +37,11 @@ public class Main {
 
         int wins = 0;
         int losses = 0;
-        String teamID = "";
         String names [] = new String[365];
         String college = "";
         String ids[] = new String[365];
-        int id = 1100;
-        //String [] listTeams = new String[364];
 
-//           id = id + 1;
-//           teamID = Integer.toString(id);
-//          listTeams[n] = teamID;
+
         for (int counter = 0; counter < 364; counter++)
         {
             String team_ID_Name = nameID[counter + 1];
@@ -63,7 +57,7 @@ public class Main {
 
         for (int k = 0; k < maxIndxTeams; k++)
         {
-            for (int i = 0; i < 5 ; i++)
+            for (int i = 0; i < maxIndx ; i++)
             {
                 String line = sStats[i + 1];
                 String [] lineItem = line.split(",");
@@ -90,17 +84,22 @@ public class Main {
                 }
             }
 
-            Team t = new Team(college,ids[k], wins,losses);
+            //Team t = new Team(college,ids[k], wins,losses);
+            int win_loss = wins - losses;
+            Team t = new Team(college, win_loss);
             stats.add(t);
 
-
-
         }
+
+
         for (int b = 0; b < stats.size(); b++)
         {
-            int wl = (stats.get(b)).calculateWinLossRatio();
-            pw.println((stats.get(b)).name + ": " + wl);
+            int winLoss = stats.get(b).win_loss_ratio;
+            String name = stats.get(b).name;
+            pw.println(winLoss + " " + name);
         }
+
+
         pw.close();
         keyIn.close();
         //System.out.println(stats);
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -5,33 +5,13 @@ package com.company;
  */
 public class Team
 {
-    private int wins;
-    private int losses;
     public String name;
-    public String ID;
-    private int win_loss_ratio;
+    public int win_loss_ratio;
 
-    public Team(String n, String i, int wTeam, int lTeam)
+    public Team(String n, int wl)
     {
-        wins = wTeam;
-        losses = lTeam;
         name = n;
-        ID = i;
-    }
-    public String returnInfoTest()
-    {
-        String print = (name + " ID: " + ID + " Number of wins: " + wins + " Number of losses: " + losses);
-        return print;
-    }
-    public int calculateWinLossRatio()
-    {
-        win_loss_ratio = wins - losses;
-        return win_loss_ratio;
-    }
-    public boolean compareTeams(Team a, Team b)
-    {
-        boolean tf = a.win_loss_ratio >= b.win_loss_ratio;
-        return tf;
+        win_loss_ratio = wl;
     }
 
 }
